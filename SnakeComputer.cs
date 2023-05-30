1926e0b1e706d208bb12a83d0b10a4104519824c
florian.hoeschl <florian.hoeschl@waygroup.de>
2023-05-30 17:26:45 +0200

    working compute shader

---
+++ SnakeComputer.cs
@@ -0,0 +1,151 @@
+using System.IO;
+using Godot;
+using Godot.Collections;
+using System;
+
+public partial class SnakeComputer : TextureRect
+{
+    [Export]
+    uint pxWidth = 512, pxHeight = 512;
+
+    RenderingDevice rd;
+    RDShaderFile shaderFile;
+    Rid shader;
+    Rid pipeline;
+    Rid arenaTex;
+    Rid uniformSet;
+    Rid snakeBuffer;
+
+    public struct SnakeData
+    {
+        public float prevPosX, prevPosY, newPosX, newPosY;
+        public float thickness;
+        public float colorR, colorG, colorB, colorA;
+        public int collision; // bool
+
+        public byte[] ToByteArray()
+        {
+            var stream = new MemoryStream();
+            var writer = new BinaryWriter(stream);
+
+            writer.Write(this.prevPosX);
+            writer.Write(this.prevPosY);
+            writer.Write(this.newPosX);
+            writer.Write(this.newPosY);
+            writer.Write(this.thickness);
+            writer.Write(this.colorR);
+            writer.Write(this.colorG);
+            writer.Write(this.colorB);
+            writer.Write(this.colorA);
+            writer.Write(this.collision);
+
+            return stream.ToArray();
+        }
+
+        public static uint SizeInByte => sizeof(float) * 9 + sizeof(int);
+    }
+
+    public SnakeComputer()
+    {
+        InitializeComputeShader();
+    }
+
+    void InitializeComputeShader()
+    {
+        // create a local rendering device.
+        rd = RenderingServer.CreateLocalRenderingDevice();
+
+        // load GLSL shader
+        shaderFile = GD.Load<RDShaderFile>("res://snakeCompute.glsl");
+        var shaderBytecode = shaderFile.GetSpirV();
+        shader = rd.ShaderCreateFromSpirV(shaderBytecode);
+
+        // Create a compute pipeline
+        pipeline = rd.ComputePipelineCreate(shader);
+
+        // create arena texture format
+        var arenaTexFormat = new RDTextureFormat();
+        arenaTexFormat.Format = RenderingDevice.DataFormat.R8G8B8A8Unorm;
+        arenaTexFormat.Width = pxWidth;
+        arenaTexFormat.Height = pxHeight;
+        arenaTexFormat.Depth = 4;
+        arenaTexFormat.UsageBits =
+            RenderingDevice.TextureUsageBits.StorageBit |
+            RenderingDevice.TextureUsageBits.CanUpdateBit |
+            RenderingDevice.TextureUsageBits.CanCopyFromBit;
+        
+        // create arena texture
+        arenaTex = rd.TextureCreate(arenaTexFormat, new RDTextureView());
+
+        // arena tex uniform
+        var arenaUniform = new RDUniform
+        {
+            UniformType = RenderingDevice.UniformType.Image,
+            Binding = 0
+        };
+        arenaUniform.AddId(arenaTex);
+
+        // create snake buffer
+        snakeBuffer = rd.StorageBufferCreate(SnakeData.SizeInByte); // max 1 snake for now
+
+        // create a snake uniform to assign the snake buffer to the rendering device
+        var snakeUniform = new RDUniform
+        {
+            UniformType = RenderingDevice.UniformType.StorageBuffer,
+            Binding = 1
+        };
+        snakeUniform.AddId(snakeBuffer);
+
+        uniformSet = rd.UniformSetCreate(new Array<RDUniform> { arenaUniform, snakeUniform }, shader, 0);
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+        ComputeSync();
+        DisplayArena();
+    }
+
+    void ComputeSync()
+    {
+        ComputeAsync();
+        rd.Sync();
+    }
+
+    void ComputeAsync()
+    {
+        // update snake data buffer
+        var snakesData = new SnakeData[] {
+            new SnakeData(){
+            prevPosX = 10,
+            prevPosY = 10,
+            newPosX = 400,
+            newPosY = 400,
+            thickness = 10,
+            colorR = 1,
+            colorG = 1,
+            colorB = 0,
+            colorA = 1,
+            collision = 0
+        } };
+        var snakesBytes = snakesData[0].ToByteArray();
+        rd.BufferUpdate(snakeBuffer, 0, SnakeData.SizeInByte * (uint)snakesData.Length, snakesBytes);
+
+        var computeList = rd.ComputeListBegin();
+        rd.ComputeListBindComputePipeline(computeList, pipeline);
+        rd.ComputeListBindUniformSet(computeList, uniformSet, 0);
+        rd.ComputeListDispatch(computeList, xGroups: pxWidth / 8, yGroups: pxHeight / 8, zGroups: 1);
+        rd.ComputeListEnd();
+
+        // force the GPU to start the commands
+        rd.Submit();
+    }
+
+    void DisplayArena()
+    {
+        var texBytes = rd.TextureGetData(arenaTex, 0);
+        var arenaImg = Image.CreateFromData((int)pxWidth, (int)pxHeight, false, Image.Format.Rgba8, texBytes);
+        var displayTex = ImageTexture.CreateFromImage(arenaImg);
+        Texture = displayTex;
+    }
+}
---
+++ snakeCompute.glsl
@@ -0,0 +1,54 @@
+#[compute]
+#version 460
+
+// Instruct the GPU to use 8x8x1 = 64 local invocations per workgroup.
+layout(local_size_x = 8, local_size_y = 8, local_size_z = 1) in;
+
+// Prepare memory for the image, which will be both read and written to
+// `restrict` is used to tell the compiler that the memory will only be accessed
+// by this variable.
+layout(r8, binding = 0) restrict uniform image2D arena;
+
+struct GLSLSnakeData
+{
+    float prevPosX, prevPosY, newPosX, newPosY;
+    float thickness;
+    float colorR, colorG, colorB, colorA;
+    int collision; // bool
+};
+
+layout(set = 0, binding = 1, std430) restrict buffer SnakeBuffer {
+    GLSLSnakeData snakes[];
+}
+snakeBuffer;
+
+float sdSegment( vec2 p, vec2 a, vec2 b )
+{
+    vec2 pa = p-a, ba = b-a;
+    float h = clamp( dot(pa,ba) / dot(ba,ba), 0.0, 1.0 );
+    return length( pa - ba*h );
+}
+
+void main() {
+	// Grab the current pixel's position from the ID of this specific invocation ("thread").
+	ivec2 coords = ivec2(gl_GlobalInvocationID.xy);
+	ivec2 dimensions = imageSize(arena);
+
+	// Even though the image format only has the red channel,
+	// this will still return a vec4: `vec4(red, 0.0, 0.0, 1.0)`
+	vec4 pixel = imageLoad(arena, coords);
+
+	for (int i = 0; i < snakeBuffer.snakes.length(); i++)
+	{
+		GLSLSnakeData snake = snakeBuffer.snakes[i];
+		vec2 prevPos = vec2(snake.prevPosX, snake.prevPosY);
+		vec2 newPos = vec2(snake.newPosX, snake.newPosY);
+		vec4 color = vec4(snake.colorR, snake.colorG, snake.colorB, snake.colorA);
+		float distToSegment = sdSegment(coords, prevPos, newPos);
+
+		if (distToSegment <= snake.thickness / 2)
+		{
+			imageStore(arena, coords, color);
+		}
+	}
+}
